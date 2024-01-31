1325e50ddfe3e515c9c1853eeee83160aa4b0052
NataliaParakletos <153620832+NataliaParakletos@users.noreply.github.com>
2024-01-31 15:31:38 +0200

    Update Avg.py

--- Avg.py
+++ Avg.py
@@ -3,12 +3,26 @@ from pathlib import Path
 with open("D:\Python\salary.txt", "w") as sal:
     sal.write("Alex Korp,3000\nNikita Borisenko,2000\nSitarama Raju,1000")
 
-with open("D:\Python\salary.txt", "r") as file1:
-    lines=file1.readlines()
-    total_salary = 0
-    for line in lines:
-        name, salary_str = line.strip().split(',')
-        salary = int(salary_str)
-        total_salary += salary
-        average_salary = total_salary / len(lines)
-    print(f"Загальна сума {total_salary}, Середня зарплата {average_salary}")
+
+def total_salary(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+            total_salary = 0
+            for line in lines:
+                name, salary_str = line.strip().split(',')
+                salary = int(salary_str)
+                total_salary += salary
+
+            average_salary = total_salary / len(lines)
+
+            return total_salary, average_salary
+
+    except FileNotFoundError:
+        return "Файл не знайдено."
+
+
+    
+total, average = total_salary("D:\Python\salary.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
